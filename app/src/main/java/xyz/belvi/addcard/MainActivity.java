77b8651078692731f8f57976bb85fbbeba7a76e9
Nosakhare Belvi <nosakharebelvi@gmail.com>
2017-07-11 09:16:25 +0100

    fixing libray module manifest bug

--- app/src/main/java/xyz/belvi/addcard/MainActivity.java
+++ app/src/main/java/xyz/belvi/addcard/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
